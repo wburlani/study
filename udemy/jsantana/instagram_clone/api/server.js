2eec7b09c27815f0a2b830114035d7db08206713
wburlani <willian.burlani@hotmail.com>
2020-06-29 15:35:09 -0300

    commit atualizar projeto instagram_clone

--- udemy/jsantana/instagram_clone/api/server.js
+++ udemy/jsantana/instagram_clone/api/server.js
@@ -1,20 +1,142 @@
 var express = require('express'),
-	bodyParser = require('body-parser'),
-	mongodb = require('mongodb');
+    bodyParser = require('body-parser'),
+    multiparty = require('connect-multiparty'),
+    mongodb = require('mongodb'),
+    objectId = require('mongodb').ObjectID,
+    fs = require('fs');
 
 var app = express();
 
-//body-parser
+// body-parser
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+app.use(multiparty());
 
 var port = 8080;
+
 app.listen(port);
 
-console.log('Servidor HTTP esta escutando na porta: ' + port);
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost',27017,{}),
+    {}
+);
 
-app.get('/', function (req, res) {
-	var resposta = {msg:'Olá Postman'};
-	res.send(resposta);
-	
+console.log('Servidor HTTP esta escutando na porta ' + port);
+
+app.get('/', function(req, res){
+    res.send({msg:'Olá'});
+});
+
+// POST(create)
+app.post('/api', function(req, res){
+    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:80');
+    //applicação cross domain habilita para qualquer domain realizar o post
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    var date = new Date();
+    var times_stamp = date.getTime();
+
+    var url_imagem = times_stamp + '_' + req.files.arquivo.originalFilename; 
+
+    var path_origem = req.files.arquivo.path;
+    var path_destino = './uploads/' + url_imagem;
+    
+
+    fs.rename(path_origem, path_destino, function(err){
+        if(err){
+            res.status(500).json({error: err});
+            return;
+        }
+
+        var dados = {
+            url_imagem: url_imagem,
+            titulo: req.body.titulo
+        }
+
+        db.open(function(err, mongoclient){
+            mongoclient.collection('postagens', function(err, collection){
+                collection.insert(dados, function(err, records){
+                    if(err){
+                        res.json({'status': 'erro'});
+                    }else{
+                        res.json({'status': 'inclusao realizada com sucesso'});
+                    }
+                    mongoclient.close();
+                });
+            });
+        });   
+    });
+});
+
+// GET(ready)
+app.get('/api', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.find().toArray(function(err, results){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+// GET by ID(ready)
+app.get('/api/:id', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.find(objectId(req.params.id)).toArray(function(err, results){
+                if(err){
+                    res.json(err);
+                }else{
+                    res.status(200).json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+// PUT by ID(update)
+app.put('/api/:id', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.update(
+                { _id : objectId(req.params.id) },
+                { $set : { titulo : req.body.titulo }},
+                {},
+                function(err, records){
+                    if(err){
+                        res.json(err);
+                    }else{
+                        res.json(records);
+                    }
+                    mongoclient.close();
+                }
+            );
+        });
+    });
+});
+
+// DELETE by ID(delete)
+app.delete('/api/:id', function(req, res){
+    db.open(function(err, mongoclient){
+        mongoclient.collection('postagens', function(err, collection){
+            collection.remove(
+                { _id : objectId(req.params.id) },
+                function(err, records){
+                    if(err){
+                        res.json(err);
+                    }else{
+                        res.json(records);
+                    }
+                    mongoclient.close();
+                }  
+            );
+        });
+    });
 });
